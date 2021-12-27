2ac479acc522697148c57abfcf04f746cdfd2c75
Muhammad Maula Irfani <89232964+maulairfani@users.noreply.github.com>
2021-12-27 22:48:35 +0700

    v3

--- app/pomodoro.py
+++ app/pomodoro.py
@@ -14,30 +14,30 @@ sebagai pengatur waktu saat belajar.
 ''')
 st.markdown('''
 ## Cara Menggunakan Pomodoro App
-1. Lengkapi data pada samping kiri terkait nama tugas, waktu fokus, waktu istirahat, dan musik pengiring
+1. Lengkapi data disamping terkait nama tugas, waktu fokus, waktu istirahat, dan musik pengiring
 2. Tekan tombol *Mulai !* untuk memulai timer
 3. Kamu dapat melihat timer berjalan dan player musik di sebelah kanan!
 4. Ulangi teknik di atas hingga empat kali
 ''')
 
-#---------------------------------------------#
+#-------------------#
 side = st.sidebar
 side.subheader('Sesuaikan pomodoro senyamanmu!')
 form = side.form('data_pomodoro')
 tugas = form.text_input('Tugas')
 t1 = form.number_input('Waktu Fokus (menit)', min_value=0, max_value=120)
+# pengulangan = form.slider('Pengulangan', min_value=1, max_value=5)
 t2 = form.number_input('Waktu Istirahat (menit)', min_value=0, max_value=25)
+# istirahat_panjang = form.number_input('Waktu Istirahat Pendek (menit)', min_value=10, max_value=60)
 pilihan_musik = ['Lo-fi Jazz', 'Lo-fi Cool']
 musik = form.radio('Musik', options=pilihan_musik)
 
 mulai = form.form_submit_button('Mulai !')
 
-#----------------------------------------------#
+#-------------------#
 
 t1 *= 60
 t2 *= 60
-
-col1, col2 = st.columns(2)
 
 def timer(t1, t2):
     with col1.empty():
@@ -51,6 +51,7 @@ def timer(t1, t2):
 
     with col1.empty():
         while t2:
+            # Start the break
             mins2, secs2 = divmod(t2, 60)
             timer2 = '{:02d}:{:02d}'.format(mins2, secs2)
             st.subheader(f"⏳ Waktu istirahat *{timer2}* ⏳")
@@ -60,15 +61,14 @@ def timer(t1, t2):
             
     
 if mulai:
-    col1.header(tugas)
+    st.header(tugas)
+    col1, col2 = st.columns(2)
     col1.write('*Tetaplah fokus hingga timer selesai!*')
-    for i in range(5):
-        col2.write('')
     col2.write('Putar musikmu disini!')
     if musik == 'Lo-fi Jazz':
         col2.audio('app/jazz.wav')
-    elif musik == 'app/Lo-fi Cool':
-        col2.audio('cool.wav') 
+    elif musik == 'Lo-fi Cool':
+        col2.audio('app/cool.wav') 
     timer(t1, t2)
     st.write('Klik tombol *Mulai!* untuk mengulangi')
     st.write('---')
