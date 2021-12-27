add6fa5cce6ce763d1c379daef7eaaa0b8faea20
Muhammad Maula Irfani <89232964+maulairfani@users.noreply.github.com>
2021-12-27 22:03:51 +0700

    v2

--- app/pomodoro.py
+++ app/pomodoro.py
@@ -2,7 +2,21 @@ import streamlit as st
 import time
 
 st.title('Pomodoro App ⏳')
+st.caption('*Developed by Kelompok 6*')
 
+st.markdown('''
+## Apa itu *pomodoro* ?
+Teknik Pomodoro adalah teknik pengaturan waktu yang telah diperkenalkan pada akhir tahun 1980 oleh Fransisco Cirillo. 
+Pomodoro sendiri dalam Bahasa Spanyol artinya tomat, Cirillo saat masih menjadi mahasiswa menggunakan kitchen timer berbentuk tomat
+sebagai pengatur waktu saat belajar.
+''')
+st.markdown('''
+## Cara Menerapkan Teknik Pomodoro
+1. Jauhi distraksi dan siapkan materi yang harus dipelajari
+2. Tentukan waktu dan mulai fokus untuk beraktivitas
+3. Lakukan kegiatan yang menyenangkan saat istirahat
+4. Ulangi teknik di atas hingga empat kali
+''')
 
 #-------------------#
 side = st.sidebar
@@ -23,48 +37,38 @@ mulai = form.form_submit_button('Mulai !')
 t1 *= 60
 t2 *= 60
 
+col1, col2, col3 = st.columns(3)
+
 def timer(t1, t2):
-    with st.empty():
+    with col1.empty():
         while t1:
             mins, secs = divmod(t1, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
-            st.subheader(f"⏳ Waktu tersisa *{timer}* ⏳")
+            st.subheader(f"⏳ Waktu fokus *{timer}* ⏳")
             time.sleep(0.05)
             t1 -= 1
             st.success("🔔 Waktu fokus selesai! Anda dapat istirahat sejenak")
 
-    with st.empty():
+    with col1.empty():
         while t2:
             # Start the break
             mins2, secs2 = divmod(t2, 60)
             timer2 = '{:02d}:{:02d}'.format(mins2, secs2)
-            st.subheader(f"⏳ Waktu tersisa *{timer2}* ⏳")
+            st.subheader(f"⏳ Waktu istirahat *{timer2}* ⏳")
             time.sleep(0.05)
             t2 -= 1
             st.error("⏰ Waktu istirahat selesai!")
             
     
 if mulai:
+    col1.title(tugas+'')
+    col1.write('''*Tetaplah fokus hingga timer selesai!*''')
+    for i in range(6):
+        col2.write('')
+    col2.write('You can play your music here!')
     if musik == 'Lo-fi Jazz':
-        st.audio('app/jazz.wav')
+        col2.audio('jazz.wav')
     elif musik == 'Lo-fi Cool':
-        st.audio('app/cool.wav')  
-    st.subheader(tugas)
-    st.write('''*Tetaplah fokus hingga timer selesai!*''')
-    timer(t1, t2)
-
-
-
-st.markdown('''
-## Apa itu *pomodoro* ?
-Teknik Pomodoro adalah teknik pengaturan waktu yang telah diperkenalkan pada akhir tahun 1980 oleh Fransisco Cirillo. 
-Pomodoro sendiri dalam Bahasa Spanyol artinya tomat, Cirillo saat masih menjadi mahasiswa menggunakan kitchen timer berbentuk tomat
-sebagai pengatur waktu saat belajar.
-''')
-st.markdown('''
-## Cara Menerapkan Teknik Pomodoro
-1. Jauhi distraksi dan siapkan materi yang harus dipelajari
-2. Tentukan waktu dan mulai fokus untuk beraktivitas
-3. Lakukan kegiatan yang menyenangkan saat istirahat
-4. Ulangi teknik di atas hingga empat kali
-''')
+        col2.audio('cool.wav') 
+    time = timer(t1, t2)
+    col1.time
