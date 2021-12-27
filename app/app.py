c4016953cb019923192d8c207da3947d19cbd154
Muhammad Maula Irfani <89232964+maulairfani@users.noreply.github.com>
2021-12-27 19:59:40 +0700

    Delete app.py

--- app/app.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-df = pd.read_csv('data.csv')
-baru = [st.text_input('mk')]
-baru = pd.Series(baru)
-df['matkul'] = df['matkul'].append(baru)
-# df.to_csv('data.csv')
-st.table(df)
