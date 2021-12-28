a8ce7396622b57c4f58d801218f5a4cc38373898
Muhammad Maula Irfani <89232964+maulairfani@users.noreply.github.com>
2021-12-28 12:32:29 +0700

    v4

--- app/pomodoro.py
+++ app/pomodoro.py
@@ -37,25 +37,25 @@ mulai = form.form_submit_button('Mulai !')
 waktu_fokus *= 60
 waktu_istirahat *= 60
 
-def timer(waktu_fokus, waktu_istirahat):
+def timer_fokus(waktu_fokus):
     with col1.empty():
         while waktu_fokus:
             mins, secs = divmod(waktu_fokus, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
-            st.subheader(f"⏳ Waktu fokus *{timer}* ⏳")
+            st.subheader(f"⏳ Waktu fokus *{timer}* ⏳")            
             time.sleep(0.05)
             waktu_fokus -= 1
             st.success("🔔 Waktu fokus selesai! Anda dapat istirahat sejenak")
 
+def timer_istirahat(waktu_istirahat):
     with col1.empty():
         while waktu_istirahat:
-            mins2, secs2 = divmod(waktu_istirahat, 60)
-            timer2 = '{:02d}:{:02d}'.format(mins2, secs2)
-            st.subheader(f"⏳ Waktu istirahat *{timer2}* ⏳")
+            mins, secs = divmod(waktu_istirahat, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            st.subheader(f"⏳ Waktu istirahat *{timer}* ⏳")            
             time.sleep(0.05)
             waktu_istirahat -= 1
             st.error("⏰ Waktu istirahat selesai!")
-            
     
 if mulai:
     st.header(tugas)
@@ -66,7 +66,8 @@ if mulai:
         col2.audio('app/jazz.wav')
     elif musik == 'Lo-fi Cool':
         col2.audio('app/cool.wav') 
-    timer(waktu_fokus, waktu_istirahat)
+    timer_fokus(waktu_fokus)
+    timer_istirahat(waktu_istirahat)
     st.write('Klik tombol *Mulai!* untuk mengulangi')
     st.write('---')
     st.caption('Terima kasih telah menggunakan pomodoro app 😊')
